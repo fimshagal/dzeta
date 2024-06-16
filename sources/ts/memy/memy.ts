06ffbdafe8d6642be192792e900ae1a04d3f5065
Andrii Yakovenko <a.yakovenko@gamzix.com>
2024-06-17 01:30:50 +0300

    tidy

--- sources/ts/memy/memy.ts
+++ sources/ts/memy/memy.ts
@@ -7,12 +7,27 @@ export class Memy implements IMemy {
     private _entries: Dictionary<MemyEntry> = {};
     private _isOverrideEntriesAllow: boolean = false;
 
+    private _isStarted: boolean = false;
+
+    private logOnStart(): void {
+        if (this._isStarted) {
+            return;
+        }
+        this._isStarted = true;
+        console.log("[Memy]: use only json-friendly values for triggers and values");
+    }
+
     public setOverrideEntriesRule(rule: boolean): Memy {
+        this.logOnStart();
+
         this._isOverrideEntriesAllow = Boolean(rule);
+
         return this;
     }
 
     public addEntry(triggers: any[], values: any[]): Memy {
+        this.logOnStart();
+
         if (!this._isOverrideEntriesAllow && this.getEntry(triggers)) {
             return this;
         }
@@ -24,6 +39,8 @@ export class Memy implements IMemy {
     }
 
     public removeEntry(triggers: any[]): Memy {
+        this.logOnStart();
+
         if (!this.getEntry(triggers)) {
             return this;
         }
@@ -36,7 +53,10 @@ export class Memy implements IMemy {
     }
 
     public getEntry(triggers: any[]): Nullable<MemyEntry> {
+        this.logOnStart();
+
         const triggersKey: string = createMemyTriggersKey(triggers);
+
         return this._entries.hasOwnProperty(triggersKey)
             ? this._entries[triggersKey]
             : null;
